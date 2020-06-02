f985173c0ec9ba2814f7fa7891ecd61064a6faaf
Anthony Yarbrough <asyarb@hawaii.edu>
2020-06-01 15:25:58 -1000

    feat: useFlexbox

--- src/theme.ts
+++ src/theme.ts
@@ -193,6 +193,75 @@ export const baseTheme: Theme = {
       'span-5': 'span 5 / span 5',
       'span-6': 'span 6 / span 6',
     },
+
+    // Flexbox
+    alignItems: {
+      center: 'center',
+      start: 'flex-start',
+      end: 'flex-end',
+      baseline: 'baseline',
+    },
+    alignContent: {
+      center: 'center',
+      start: 'flex-start',
+      end: 'flex-end',
+      baseline: 'baseline',
+    },
+    alignSelf: {
+      center: 'center',
+      start: 'flex-start',
+      end: 'flex-end',
+      baseline: 'baseline',
+    },
+    justifyItems: {
+      center: 'center',
+      start: 'flex-start',
+      end: 'flex-end',
+    },
+    justifyContent: {
+      center: 'center',
+      start: 'flex-start',
+      end: 'flex-end',
+      spaceBetween: 'space-between',
+    },
+    justifySelf: {
+      center: 'center',
+      start: 'flex-start',
+      end: 'flex-end',
+    },
+    flexWrap: {
+      wrap: 'wrap',
+      nowrap: 'nowrap',
+    },
+    flexDirection: {
+      row: 'row',
+      rowReverse: 'row-reverse',
+      column: 'column',
+      columnReverse: 'column-reverse',
+    },
+    flexGrow: {
+      0: 0,
+      1: 1,
+    },
+    flexShrink: {
+      0: 0,
+      1: 1,
+    },
+    flex: {
+      equal0: '1 1 0px',
+    },
+    flexBasis: {
+      0: '0px',
+      1: '10%',
+      2: '20%',
+      3: '30%',
+      4: '40%',
+      5: '50%',
+      6: '60%',
+      7: '70%',
+      8: '80%',
+      auto: 'auto',
+    },
   },
 }
 
--- src/useFlexboxStyles.treat.ts
+++ src/useFlexboxStyles.treat.ts
@@ -3,290 +3,225 @@ import { pipe } from 'fp-ts/es6/pipeable'
 
 import { mapToProperty, mapToResponsive } from './utils'
 
-export const alignItemsRules = {
-  center: 'center',
-  start: 'flex-start',
-  end: 'flex-end',
-  baseline: 'baseline',
-}
 export const alignItems = styleMap((theme) =>
   pipe(
-    alignItemsRules,
+    theme.rules.alignItems,
     mapToProperty('alignItems'),
     mapToResponsive('mobile', theme),
   ),
 )
 export const alignItemsTablet = styleMap((theme) =>
   pipe(
-    alignItemsRules,
+    theme.rules.alignItems,
     mapToProperty('alignItems'),
     mapToResponsive('tablet', theme),
   ),
 )
 export const alignItemsDesktop = styleMap((theme) =>
   pipe(
-    alignItemsRules,
+    theme.rules.alignItems,
     mapToProperty('alignItems'),
     mapToResponsive('desktop', theme),
   ),
 )
 export const alignItemsDesktopWide = styleMap((theme) =>
   pipe(
-    alignItemsRules,
+    theme.rules.alignItems,
     mapToProperty('alignItems'),
     mapToResponsive('desktopWide', theme),
   ),
 )
 
-const alignContentRules = {
-  center: 'center',
-  start: 'flex-start',
-  end: 'flex-end',
-  baseline: 'baseline',
-}
 export const alignContent = styleMap((theme) =>
   pipe(
-    alignContentRules,
+    theme.rules.alignContent,
     mapToProperty('alignContent'),
     mapToResponsive('mobile', theme),
   ),
 )
 export const alignContentTablet = styleMap((theme) =>
   pipe(
-    alignContentRules,
+    theme.rules.alignContent,
     mapToProperty('alignContent'),
     mapToResponsive('tablet', theme),
   ),
 )
 export const alignContentDesktop = styleMap((theme) =>
   pipe(
-    alignContentRules,
+    theme.rules.alignContent,
     mapToProperty('alignContent'),
     mapToResponsive('desktop', theme),
   ),
 )
 export const alignContentDesktopWide = styleMap((theme) =>
   pipe(
-    alignContentRules,
+    theme.rules.alignContent,
     mapToProperty('alignContent'),
     mapToResponsive('desktopWide', theme),
   ),
 )
 
-const alignSelfRules = {
-  center: 'center',
-  start: 'flex-start',
-  end: 'flex-end',
-  baseline: 'baseline',
-}
 export const alignSelf = styleMap((theme) =>
   pipe(
-    alignSelfRules,
+    theme.rules.alignSelf,
     mapToProperty('alignSelf'),
     mapToResponsive('mobile', theme),
   ),
 )
 export const alignSelfTablet = styleMap((theme) =>
   pipe(
-    alignSelfRules,
+    theme.rules.alignSelf,
     mapToProperty('alignSelf'),
     mapToResponsive('tablet', theme),
   ),
 )
 export const alignSelfDesktop = styleMap((theme) =>
   pipe(
-    alignSelfRules,
+    theme.rules.alignSelf,
     mapToProperty('alignSelf'),
     mapToResponsive('desktop', theme),
   ),
 )
 export const alignSelfDesktopWide = styleMap((theme) =>
   pipe(
-    alignSelfRules,
+    theme.rules.alignSelf,
     mapToProperty('alignSelf'),
     mapToResponsive('desktopWide', theme),
   ),
 )
 
-export const justifyItemsRules = {
-  center: 'center',
-  start: 'flex-start',
-  end: 'flex-end',
-}
 export const justifyItems = styleMap((theme) =>
   pipe(
-    justifyItemsRules,
+    theme.rules.justifyItems,
     mapToProperty('justifyItems'),
     mapToResponsive('mobile', theme),
   ),
 )
 export const justifyItemsTablet = styleMap((theme) =>
   pipe(
-    justifyItemsRules,
+    theme.rules.justifyItems,
     mapToProperty('justifyItems'),
     mapToResponsive('tablet', theme),
   ),
 )
 export const justifyItemsDesktop = styleMap((theme) =>
   pipe(
-    justifyItemsRules,
+    theme.rules.justifyItems,
     mapToProperty('justifyItems'),
     mapToResponsive('desktop', theme),
   ),
 )
 export const justifyItemsDesktopWide = styleMap((theme) =>
   pipe(
-    justifyItemsRules,
+    theme.rules.justifyItems,
     mapToProperty('justifyItems'),
     mapToResponsive('desktopWide', theme),
   ),
 )
 
-export const justifyContentRules = {
-  center: 'center',
-  start: 'flex-start',
-  end: 'flex-end',
-  spaceBetween: 'space-between',
-}
 export const justifyContent = styleMap((theme) =>
   pipe(
-    justifyContentRules,
+    theme.rules.justifyContent,
     mapToProperty('justifyContent'),
     mapToResponsive('mobile', theme),
   ),
 )
 export const justifyContentTablet = styleMap((theme) =>
   pipe(
-    justifyContentRules,
+    theme.rules.justifyContent,
     mapToProperty('justifyContent'),
     mapToResponsive('tablet', theme),
   ),
 )
 export const justifyContentDesktop = styleMap((theme) =>
   pipe(
-    justifyContentRules,
+    theme.rules.justifyContent,
     mapToProperty('justifyContent'),
     mapToResponsive('desktop', theme),
   ),
 )
 export const justifyContentDesktopWide = styleMap((theme) =>
   pipe(
-    justifyContentRules,
+    theme.rules.justifyContent,
     mapToProperty('justifyContent'),
     mapToResponsive('desktopWide', theme),
   ),
 )
 
-const justifySelfRules = {
-  center: 'center',
-  start: 'flex-start',
-  end: 'flex-end',
-}
 export const justifySelf = styleMap((theme) =>
   pipe(
-    justifySelfRules,
+    theme.rules.justifySelf,
     mapToProperty('justifySelf'),
     mapToResponsive('mobile', theme),
   ),
 )
 export const justifySelfTablet = styleMap((theme) =>
   pipe(
-    justifySelfRules,
+    theme.rules.justifySelf,
     mapToProperty('justifySelf'),
     mapToResponsive('tablet', theme),
   ),
 )
 export const justifySelfDesktop = styleMap((theme) =>
   pipe(
-    justifySelfRules,
+    theme.rules.justifySelf,
     mapToProperty('justifySelf'),
     mapToResponsive('desktop', theme),
   ),
 )
 export const justifySelfDesktopWide = styleMap((theme) =>
   pipe(
-    justifySelfRules,
+    theme.rules.justifySelf,
     mapToProperty('justifySelf'),
     mapToResponsive('desktopWide', theme),
   ),
 )
 
-const flexWrapRules = {
-  wrap: 'wrap',
-  nowrap: 'nowrap',
-}
 export const flexWrap = styleMap((theme) =>
-  pipe(flexWrapRules, mapToProperty('flexWrap')),
+  pipe(theme.rules.flexWrap, mapToProperty('flexWrap')),
 )
 
-const flexDirectionRules = {
-  row: 'row',
-  rowReverse: 'row-reverse',
-  column: 'column',
-  columnReverse: 'column-reverse',
-}
 export const flexDirection = styleMap((theme) =>
   pipe(
-    flexDirectionRules,
+    theme.rules.flexDirection,
     mapToProperty('flexDirection'),
     mapToResponsive('mobile', theme),
   ),
 )
 export const flexDirectionTablet = styleMap((theme) =>
   pipe(
-    flexDirectionRules,
+    theme.rules.flexDirection,
     mapToProperty('flexDirection'),
     mapToResponsive('tablet', theme),
   ),
 )
 export const flexDirectionDesktop = styleMap((theme) =>
   pipe(
-    flexDirectionRules,
+    theme.rules.flexDirection,
     mapToProperty('flexDirection'),
     mapToResponsive('desktop', theme),
   ),
 )
 export const flexDirectionDesktopWide = styleMap((theme) =>
   pipe(
-    flexDirectionRules,
+    theme.rules.flexDirection,
     mapToProperty('flexDirection'),
     mapToResponsive('desktopWide', theme),
   ),
 )
 
-const flexGrowRules = {
-  0: 0,
-  1: 1,
-}
 export const flexGrow = styleMap((theme) =>
-  pipe(flexGrowRules, mapToProperty('flexGrow')),
+  pipe(theme.rules.flexGrow, mapToProperty('flexGrow')),
 )
 
-const flexShrinkRules = {
-  0: 0,
-  1: 1,
-}
 export const flexShrink = styleMap((theme) =>
-  pipe(flexShrinkRules, mapToProperty('flexShrink')),
+  pipe(theme.rules.flexShrink, mapToProperty('flexShrink')),
 )
 
-const flexRules = {
-  equal0: '1 1 0px',
-}
-export const flex = styleMap((theme) => pipe(flexRules, mapToProperty('flex')))
+export const flex = styleMap((theme) =>
+  pipe(theme.rules.flex, mapToProperty('flex')),
+)
 
-const flexBasisRules = {
-  0: '0px',
-  1: '10%',
-  2: '20%',
-  3: '30%',
-  4: '40%',
-  5: '50%',
-  6: '60%',
-  7: '70%',
-  8: '80%',
-  auto: 'auto',
-}
 export const flexBasis = styleMap((theme) =>
-  pipe(flexBasisRules, mapToProperty('flexBasis')),
+  pipe(theme.rules.flexBasis, mapToProperty('flexBasis')),
 )
