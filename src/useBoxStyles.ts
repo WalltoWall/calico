013a99f878aaacbb00c3ee799d898460752ad776
Anthony Yarbrough <asyarb@hawaii.edu>
2020-06-09 09:52:58 -1000

    refactor: remove intermediary hooks

--- src/useBoxStyles.ts
+++ src/useBoxStyles.ts
@@ -7,32 +7,8 @@ import { ResponsiveProp } from './types'
 
 import * as treatStyleRefs from './useBoxStyles.treat'
 
-type UseBaseBoxStylesProps = {
+type BaseBoxStylesProps = {
   [K in keyof Theme['rules']]?: ResponsiveProp<keyof Theme['rules'][K]>
-}
-
-const resolveClassNames = (props: UseBaseBoxStylesProps, styles: any) => {
-  let resolvedClassNames: (string | undefined)[] = []
-
-  for (const key in props) {
-    const value = props[key as keyof Theme['rules']]
-    if (!value) continue
-
-    resolvedClassNames.push(
-      resolveResponsiveProp<string | number>(
-        value,
-        styles[key as keyof Theme['rules']],
-      ),
-    )
-  }
-
-  return resolvedClassNames
-}
-
-const useBaseBoxStyles = (props: UseBaseBoxStylesProps = {}) => {
-  const styleRefs = useStyles(treatStyleRefs)
-
-  return clsx(resolveClassNames(props, styleRefs.styles))
 }
 
 type NotUndefOrNever<T extends {}> = Pick<
@@ -50,12 +26,6 @@ type BoxHoverProp = NotUndefOrNever<
   }
 >
 
-const useHoverStyles = (props: BoxHoverProp = {}) => {
-  const styleRefs = useStyles(treatStyleRefs)
-
-  return clsx(resolveClassNames(props, styleRefs.hoverStyles))
-}
-
 type BoxFocusProp = NotUndefOrNever<
   {
     [K in keyof Theme['variants']]?: NonNullable<
@@ -66,21 +36,37 @@ type BoxFocusProp = NotUndefOrNever<
   }
 >
 
-const useFocusStyles = (props: BoxFocusProp = {}) => {
-  const styleRefs = useStyles(treatStyleRefs)
+const resolveClassNames = (
+  props: BaseBoxStylesProps | undefined,
+  styles: any,
+) => {
+  if (props === undefined) return
 
-  return clsx(resolveClassNames(props, styleRefs.focusStyles))
+  let resolvedClassNames: (string | undefined)[] = []
+
+  for (const key in props) {
+    const value = props[key as keyof Theme['rules']]
+    if (!value) continue
+
+    resolvedClassNames.push(
+      resolveResponsiveProp(value, styles[key as keyof Theme['rules']]),
+    )
+  }
+
+  return resolvedClassNames
 }
 
 export type UseBoxStylesProps = {
   hover?: BoxHoverProp
   focus?: BoxFocusProp
-} & UseBaseBoxStylesProps
+} & BaseBoxStylesProps
 
 export const useBoxStyles = ({ hover, focus, ...props }: UseBoxStylesProps) => {
-  const baseClassNames = useBaseBoxStyles(props)
-  const hoverClassNames = useHoverStyles(hover)
-  const focusClassNames = useFocusStyles(focus)
+  const styleRefs = useStyles(treatStyleRefs)
 
-  return clsx(baseClassNames, hoverClassNames, focusClassNames)
+  return clsx(
+    resolveClassNames(props, styleRefs.styles),
+    resolveClassNames(hover, styleRefs.hoverStyles),
+    resolveClassNames(focus, styleRefs.focusStyles),
+  )
 }
