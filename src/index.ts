caf8bdbeeb11db8922af67f7d79c584f833dfb4c
Anthony Yarbrough <asyarb@hawaii.edu>
2020-06-09 15:25:26 -1000

    docs: api exports and example

--- src/index.ts
+++ src/index.ts
@@ -4,9 +4,9 @@ export type { BoxProps } from './Box'
 export { useBoxStyles } from './useBoxStyles'
 export type { UseBoxStylesProps } from './useBoxStyles'
 
+export { createCalicoTheme, baseCalicoTheme } from './createCalicoTheme'
 export { mapFontsets } from './mapFontsets'
 export { createMq } from './createMq'
-export { createCalicoTheme, baseCalicoTheme } from './createCalicoTheme'
 export type { CalicoTheme } from './createCalicoTheme'
 
 export * from './types'
