6c19b0da20b11bb95230962cd1509405dab4989d
Angelo Ashmore <angeloashmore@users.noreply.github.com>
2020-11-28 16:59:05 -1000

    test: use simpler $eval fn to get className

--- test/utils/getClassNames.ts
+++ test/utils/getClassNames.ts
@@ -1,12 +1,4 @@
 import { Page } from 'puppeteer'
 
 export const getClassNames = async (page: Page, selector: string) =>
-  await page.evaluate(
-    ({ selector }) => {
-      const el = document.querySelector(selector)
-      if (!el) return undefined
-
-      return el.className
-    },
-    { selector },
-  )
+  await page.$eval(selector, (el) => el.className)
